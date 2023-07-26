9de338eaa5c2d6001141431e31d0347b9588048d
Michael <odinetsmichael@gmail.com>
2023-07-26 14:43:27 +0300

    WIP

---
+++ vue-project/src/components/StartPage.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <div class="greetings">
+      <h1 class="welcome">Вас вітає UaLiveRPG</h1>
+  </div>
+</template>
+
+<style scoped>
+.greetings{
+  width: 100%;
+  margin: 0 auto;
+}
+.welcome{
+  text-align: center;
+}
+</style>
