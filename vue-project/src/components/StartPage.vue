5ca14e7caeb3991dc130c648fa9540b2bfd7a93e
Michael <odinetsmichael@gmail.com>
2023-07-26 15:01:25 +0300

    Create StartPage

--- vue-project/src/components/StartPage.vue
+++ vue-project/src/components/StartPage.vue
@@ -2,15 +2,24 @@
 <template>
   <div class="greetings">
       <h1 class="welcome">Вас вітає UaLiveRPG</h1>
+      
+      <button class="btn btn-success start-button">Увійти</button>
   </div>
 </template>
 
 <style scoped>
 .greetings{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   width: 100%;
-  margin: 0 auto;
 }
 .welcome{
   text-align: center;
 }
+.start-button{
+  margin-top: 15px;
+}
+
 </style>
