a2fec9c1ea83f614696aedafbac009881df55e90
Michael <odinetsmichael@gmail.com>
2023-07-26 18:58:36 +0300

    added FormulateInput library

--- UaLiveRPG/src/components/CreatingCharacter.vue
+++ UaLiveRPG/src/components/CreatingCharacter.vue
@@ -1,5 +1,29 @@
 <template>
     <div>
-        priv
+        <FormulateInput
+            type="text"
+            name="name"
+            label="Введіть ваше ім`я"
+            v-model="name"
+        />
+        <div>{{name}}</div>
     </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: ''
+    }
+  },
+
+  computed: {
+    }
+}
+
+</script>
+
+<style>
+
+</style>
--- UaLiveRPG/src/main.js
+++ UaLiveRPG/src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router/router'
+import VueFormulate from '@braid/vue-formulate'
+Vue.use(VueFormulate)
 
 import './assets/main.css'
 
